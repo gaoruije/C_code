d30ae108578c6962309bdb8ac38265d181be39b9
高瑞杰 <3131114593@qq.com>
2020-10-15 22:36:28 +0800

    c语言代码练习

---
+++ test_09_27/test_09_27/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	char str[10] = "hello";
+	char str1[10] = { 'h', 'e', 'l', 'l', 'o' };
+	printf("%s\n", str1);
+	system("pause");
+	return 0;
+}
